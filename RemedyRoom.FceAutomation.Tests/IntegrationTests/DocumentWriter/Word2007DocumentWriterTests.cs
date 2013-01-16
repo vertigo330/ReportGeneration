d033671c9f5251d8d444a91c6ed9da9dad4c4636
Jonathan Patton <jonathan.c.patton@gmail.com>
2013-01-16 16:19:34 +0200

    - Created empty unit test for the Word2007DocumentWriter

---
+++ RemedyRoom.FceAutomation.Tests/IntegrationTests/DocumentWriter/Word2007DocumentWriterTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RemedyRoom.FceAutomation.Tests.IntegrationTests.DocumentWriter
+{
+    [TestClass]
+    public class Word2007DocumentWriterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
